66318f0ac7f77c7513d5fbbe02e156de4d0b8433
jiaorpeng <469067683@qq.com>
2020-05-29 17:53:16 +0800

    blog

---
+++ js/url.js
@@ -0,0 +1,29 @@
+var hostUrl = 'http://127.0.0.1:8888/blog/';
+// var hostUrl = 'http://127.0.0.1:8888/blog/';
+
+
+
+
+
+/*$.ajaxSetup({
+	headers : {
+		'Content-Type' : 'application/json;charset=utf8',
+		'AUTHORIZATION' : localStorage.getItem('token')
+	},
+	
+	
+	complete : function(XMLHttpRequest, textStatus) {
+		// 通过XMLHttpRequest取得响应头，sessionstatus，
+		
+		console.log(XMLHttpRequest.responseJSON)
+		console.log(textStatus)
+//		var sessionstatus = XMLHttpRequest.getResponseHeader("sessionstatus");
+//		if (sessionstatus == "timeout") {
+//			// 如果超时就处理 ，指定要跳转的页面(比如登陆页)
+//			window.location.replace("/login/index.php");
+//		}
+	},
+});
+
+
+*/
